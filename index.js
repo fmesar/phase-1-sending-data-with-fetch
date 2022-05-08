8ed7233a047160227b86d078819068080bf169db
Francisco Mesar <fmesar87@gmail.com>
2022-05-08 15:55:16 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
+
+  function submitData(name, email) {
 const formData = {
-    dogName: "Byron",
-    dogBreed: "Poodle",
+    name: name,
+    email: email,
+   
   };
-  
+
+
+
+  // method: "POST" is missing from the object below
   const configurationObject = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
     },
-    body: JSON.stringify(formData),
+    body: JSON.stringify(formData)
+   
   };
-  
-  fetch("http://localhost:3000/dogs", configurationObject)
+
+
+ return fetch("http://localhost:3000/users", configurationObject)
     .then(function (response) {
       return response.json();
     })
     .then(function (object) {
       console.log(object);
-    });
+      document.body.innerHTML = ` ${object.id}` 
+    })
+    .catch ((error) => {
+        //console.log(error)
+        console.log(error.message)
+        document.body.innerHTML = error.message
+})}
+//     function ElementCreator(data){
+//         let elements = document.createElement('div')
+
+//         elements.innerHTML = `
+       
+//         <ul class="list-group list-group-flush">
+//     <li class="list-group-item">${data.name}</li>
+//     <li class="list-group-item">${data.email}</li>          
+//   </ul>
+// `
+//  let data = document.querySelector('body')
+//     data.innerHTML = `${formData.name}, ${formData.email}`
+    // }
+    // ElementCreator(formData)
